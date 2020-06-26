7ca95d3a8147e8d63093f4da736a4ad037c076dd
VanillaGorilla <adrianlrivers@gmail.com>
2020-06-26 10:09:19 +0200

    bug/fix-screenshot-image-cutt-off-bug (#5)
    * Fix cut off screenshot issue, add large element stories for all viewports
    
    * Make colours more obvious in large element story
    
    * Version bump
    
    * Fixup stories

--- playground/stories/viewports.stories.js
+++ playground/stories/viewports.stories.js
@@ -21,3 +21,54 @@ SmallViewport.story = {
     },
   },
 };
+
+const largeElementTemplate = `
+<div>
+  <div 
+    class="large-element" 
+    style="
+    background: #495057; 
+    width: 500px; 
+    height: 2000px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-top: 10px solid #3EB62B;
+    border-bottom: 10px solid #55ACEE;
+  ">
+  </div>
+</div>`;
+
+export const LargeElementDefaultViewport = () => largeElementTemplate;
+LargeElementDefaultViewport.story = {
+  parameters: {
+    imageSnapshots: {
+      selector: '.large-element',
+    },
+  },
+};
+
+export const LargeElementMediumViewport = () => largeElementTemplate;
+LargeElementMediumViewport.story = {
+  parameters: {
+    imageSnapshots: {
+      selector: '.large-element',
+    },
+    viewport: {
+      defaultViewport: 'medium',
+    },
+  },
+};
+
+export const LargeElementSmallViewport = () => largeElementTemplate;
+LargeElementSmallViewport.story = {
+  parameters: {
+    imageSnapshots: {
+      selector: '.large-element',
+    },
+    viewport: {
+      defaultViewport: 'small',
+    },
+  },
+};
